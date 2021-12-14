660da109472152a30fc30719daeb89e28914f734
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2021-12-14 14:03:39 +0100

    add about lidia

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+console.log('first output');
+console.log('first ',' ouput');
+console.log("ciao ',' mamma ");
+
+console.log('ciao1 \':\' mamma1');
+console.log("ciao 2',' mamma2 ");
+console.log("ciao 3\":\" mamma 3");
+console.log(2 + 3);
+console.log('hello wrld');
+var age = 29;
+console.log(age);
+var name = 'oksana';
+name = 'roma';
+console.log(name, ' ha ', age, ' anni');//variabile separata num string
+console.log(name + ' ha '+ age + ' anni');// variabile unita stringa
+console.clear();
+if (age > 50){
+    console.log('tu sei piu vecchio di 40')
+} else{
+    console.log('tu sei piu giovane di  50')
+}
+
